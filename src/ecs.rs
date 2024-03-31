da40c29a398a5dddc1444fdc317bbd9ae3b2c5ac
Jakob Flocke <47260919+JakobFlocke@users.noreply.github.com>
2024-03-31 10:45:03 +0200

    curr implementing movement & stats

--- src/ecs.rs
+++ src/ecs.rs
@@ -5,47 +5,8 @@ use raylib::prelude::*;
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Entity(pub u32);
 
-/* Component Data */
-#[derive(Debug)]
-pub struct Health(pub f32);
-
-#[derive(Debug)]
-pub struct Transform {
-    pub pos: [f32; 2],
-    pub rot: [f32; 2],
-    pub scale: [f32; 2],
-}
-
-#[derive(Debug)]
-pub struct Rect {
-    pub w: f32,
-    pub h: f32,
-}
-
 #[derive(Debug)]
 pub struct Sprite(pub Texture2D);
-
-/* Systems */
-pub fn move_system(transform: &mut Transform) {
-    // Movement logic
-}
-
-pub fn render_system(components: &Vec<Component>, d: &mut RaylibDrawHandle<'_>) {
-    for component in components {
-        match component {
-            Component::Transform(_) => {} // Handle transform rendering if needed
-            Component::Health(_) => {} // Handle health rendering if needed
-            Component::Rectangle(rectangle) => {
-                // Rendering logic for rectangle
-                d.draw_rectangle(300, 300, rectangle.w as i32, rectangle.h as i32, Color::WHITE);
-            }
-            Component::Sprite(sprite) => {
-                // Rendering logic for sprite
-                println!("Rendering sprite: {:?}", sprite);
-            }
-        }
-    }
-}
 
 /* World */
 pub struct World {
@@ -86,4 +47,53 @@ pub enum Component {
     Health(Health),
     Rectangle(Rect),
     Sprite(Sprite),
+    Movement(Movement),
+}
+
+/* Component Data */
+#[derive(Debug)]
+pub struct Health(pub f32);
+
+#[derive(Debug)]
+pub struct Transform {
+    pub pos: [f32; 2],
+    pub rot: [f32; 2],
+    pub scale: [f32; 2],
+}
+
+#[derive(Debug)]
+pub struct Rect {
+    pub w: f32,
+    pub h: f32,
+}
+
+pub struct Movement {
+    // requires transform
+    pub dir: [f32;2],
+}
+
+pub struct Stats {
+    
+}
+
+/* Systems */
+pub fn movement_system(components: &Vec<Component>) {
+    // Movement logic
+    
+}
+
+pub fn render_system(components: &Vec<Component>, d: &mut RaylibDrawHandle<'_>) {
+    for component in components {
+        match component {
+            Component::Rectangle(rectangle) => {
+                // Rendering logic for rectangle
+                d.draw_rectangle(300, 300, rectangle.w as i32, rectangle.h as i32, Color::WHITE);
+            }
+            Component::Sprite(sprite) => {
+                // Rendering logic for sprite
+                println!("Rendering sprite: {:?}", sprite);
+            }
+            _ => {}
+        }
+    }
 }
